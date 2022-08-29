8ae71b25fc8ac82681f5ef115e8c5aa781a91523
Oleksandr Nashyvan <on@waterfall.foundation>
2022-08-29 20:31:06 +0200

    remove  IERC20 for IRP_ERC20

--- RP_ERC20/contracts/IRP_ERC20.sol
+++ RP_ERC20/contracts/IRP_ERC20.sol
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
-
 interface IRP_ERC20 {
     enum RegularPaymentInterval {Second, Minute, Hour, Day, Week, Month, Year}
 
